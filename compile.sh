d11147fc7655af59b01447474768f37f91060050
Garrett W <garrettw87@gmail.com>
2016-12-01 00:20:25 +0000

    added old IE fixes into main code since they weren't many

--- compile.sh
+++ compile.sh
@@ -1,8 +1,2 @@
 scss scss/_vitals.scss css/vitals.css --style expanded
-scss scss/_vitals.scss min-css/vitals.css --style compressed
-
-scss scss/_ie9down.scss css/ie9down.css --style expanded
-scss scss/_ie9down.scss min-css/ie9down.css --style compressed
-
-scss scss/_ie10down.scss css/ie10down.css --style expanded
-scss scss/_ie10down.scss min-css/ie10down.css --style compressed
+scss scss/_vitals.scss css/vitals.min.css --style compressed
