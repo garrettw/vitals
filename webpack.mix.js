f72423f016861bf34bee34256aaa882627466214
Garrett W <garrett@encoremultimedia.com>
2020-01-24 10:17:49 -0600

    updated deps, version bump

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,10 @@
-const Fiber = require('fibers');
-
 let mix = require('laravel-mix');
 
 mix.sourceMaps(false, 'source-map');
 
 // Compile
 if (mix.inProduction()) {
-  mix.sass('scss/_vitals.scss', 'css/vitals.min.css', { fiber: Fiber });
+  mix.sass('scss/_vitals.scss', 'css/vitals.min.css');
 } else {
-  mix.sass('scss/_vitals.scss', 'css/vitals.css', { fiber: Fiber });
+  mix.sass('scss/_vitals.scss', 'css/vitals.css');
 }
